b2b1ad2c66f624df64fd83bfa724712bba542ee8
ThiagoCSgit <57235729+ThiagoCSgit@users.noreply.github.com>
2024-11-13 13:22:45 -0300

    Add files via upload

---
+++ Controllers/AlunosController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AppMvcFuncional.Data;
+using AppMvcFuncional.Models;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AppMvcFuncional.Controllers
+{
+    [Authorize]
+    [Route("meus-alunos")]
+    public class AlunosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AlunosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Alunos
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            //dura somente 1 request
+            ViewBag.Sucesso = "Listagem bem sucedida";
+            return _context.Aluno != null ? View(await _context.Aluno.ToListAsync()) : Problem("A tabela Aluno não existe");
+        }
+
+        // GET: Alunos/Details/5
+        [Route("{id:int}/detalhes")]
+        public async Task<IActionResult> Details(int id)
+        {
+            if (_context.Aluno == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Aluno
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return View(aluno);
+        }
+
+        // GET: Alunos/Create
+        [Route("novo")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Alunos/Create
+        [HttpPost("novo")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,DataNascimento,Email,EmailConfirmacao,Avaliacao,Ativo")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(aluno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+
+        // GET: Alunos/Edit/5
+        [Route("editar/{id:int}")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (_context.Aluno == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Aluno.FindAsync(id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+            return View(aluno);
+        }
+
+        // POST: Alunos/Edit/5
+        [HttpPost("editar/{id:int}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,DataNascimento,Email,Avaliacao,Ativo")] Aluno aluno)
+        {
+            if (id != aluno.Id)
+            {
+                return NotFound();
+            }
+            ModelState.Remove("EmailConfirmacao");
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(aluno);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlunoExists(aluno.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                //dura enquanto não for lido, mas ao ser jogado na View e o usuário fazer um novo request o TempData é perdido
+                TempData["Sucesso"] = "Aluno editado com sucesso";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+
+        // GET: Alunos/Delete/5
+        [Route("excluir/{id:int}")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Aluno == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Aluno
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return View(aluno);
+        }
+
+        // POST: Alunos/Delete/5
+        //ActionName define qual é o nome da action, com já existe uma aciotn chamada Delete que recebe o mesmo parametro, essa foi chamada de DeleteConfirmed
+        //porém o aspnet não vai saber que essa action seria de Delete, por isso foi usado o ActionName("Delete") para informar a ação desse método.
+        [HttpPost("excluir/{id:int}"), ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var aluno = await _context.Aluno.FindAsync(id);
+            if (aluno != null)
+            {
+                _context.Aluno.Remove(aluno);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlunoExists(int id)
+        {
+            return _context.Aluno.Any(e => e.Id == id);
+        }
+    }
+}
